05fcab81d743b4e1f50cb7daa363a8cc1b244db6
Torbjorn Eklov <torbjorn.eklov@interlan.se>
2021-04-20 07:21:47 +0200

    Update pmtu.sh

--- pmtu.sh
+++ pmtu.sh
@@ -1,32 +1,32 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # usage: pmtu.sh interlan.se interlan.se 2001:db8:100:193::1145 https
 
 # Start here!
 
-if [ -z "$1" ] ; then
+if [ -z "${1}" ] ; then
         echo "ERROR: $0 sitename"
         exit 10
 fi
 
-uri=$1
-domain=$2
-srcaddr=$3
-http=$4
+uri=${1}
+domain=${2}
+srcaddr=${3}
+http=${4}
 
 
 echo "INFO: Looking for AAAA RR at $domain...."
-ipv6=`dig -taaaa +short $domain| grep :`
+ipv6=$(dig -taaaa +short $domain| grep :)
 #sleep 0.5
 
 
 # Check if IPv6 exist
-if [ ! "$ipv6" ] ; then
+if [ ! "${ipv6}" ] ; then
                 echo "ERROR: $domain dont have IPv6"
 		echo "INFO: Please contact webbmaster for $domain to enable IPv6 support"
 		echo "INFO: Test ended $(date)"
 		exit
 fi
-if [ "$ipv6" = "2001:db8:102:193:100::10" ] ; then
+if [ "${ipv6}" = "2001:db8:1::10" ] ; then
 	echo "INFO: This host is located on same server as ipv6alizer.se and can't be tested"
 	echo "INFO: We need more servers  - sorry..."
 	echo "INFO: Test ended $(date)"
@@ -37,8 +37,8 @@ fi
 echo "INFO: test started `date`"
 
 curl -6 -s -q -m3 "${4}://${domain}" >/dev/null
-exit="$?"
-        case "$exit" in
+exit="${?}"
+        case "${exit}" in
         0|1)
 		#echo "INFO: has functional IPv6" ;;
 		;;
@@ -57,11 +57,11 @@ exit="$?"
 		exit ;;
 	esac
 
-number=`echo $ipv6|wc -w`
-testaddress=`echo $ipv6 | awk '{ print $1 }'`
+number=$(echo $ipv6|wc -w)
+testaddress=$(echo $ipv6 | awk '{ print $1 }')
 
 echo "INFO: $domain have $number ipv6 address{es}"
-if [ "$number" -gt "1"  ] ; then
+if [ "${number}" -gt "1"  ] ; then
 	#echo "INFO: If all addresses is behind same loadbalancer the second test is probably unreliable"
 	echo "INFO: we only test $testaddress "
 fi
@@ -73,28 +73,28 @@ echo
 	end=$(shuf -i100-199 -n1)
 
         echo "INFO: running scamper  -I \"tbit -M 1280 -t pmtud -S 2001:db8:102:193:100::${end} -u $http://$uri $testaddress\"" 
-	sudo scamper  -I "tbit -M 1280 -t pmtud -S 2001:db8:102:193:100::${end} -u $http://$uri $testaddress" >>/tmp/scamper.$$
+	sudo scamper  -I "tbit -M 1280 -t pmtud -S 2001:db8:1::${end} -u $http://$uri $testaddress" >>/tmp/scamper.$$
 	result=`grep "result:" /tmp/scamper.$$`
 
 
-        echo "INFO: $result"
-        if [ "`echo $result | grep toosmall`"  ]; then
+        echo "INFO: ${result}"
+        if [ "`echo ${result} | grep toosmall)"  ]; then
                 echo "WARNING: $4://$domain payload to small for test"
 		echo "INFO: Probably a redirect, try to enable https and/or add/remove www."
         fi
-        if [ "`echo $result | grep success`"  ]; then
+        if [ "$(echo ${result} | grep success)"  ]; then
                 echo "NOTICE: $4://$domain is responding to PTB correctly"
         fi
-        if [ "`echo $result | grep nodata`"  ]; then
+        if [ "$(echo ${result} | grep nodata)"  ]; then
                  echo "ERROR: $4://$domain didn't test correct, try again in 30 minutes"
         fi
-        if [ "`echo $result | grep pmtud-fail`"  ]; then
+        if [ "$(echo ${result} | grep pmtud-fail)"  ]; then
                  echo "ERROR: $4://$domain don't listen to PTB "
         fi
-        if [ "`echo $result | grep tcp-noconn`"  ]; then
+        if [ "$(echo ${result} | grep tcp-noconn)"  ]; then
                  echo "ERROR: $4://$domain is broken and don't listen to IPv6 for $http://$uri"
         fi
-        if [ "`echo $result | grep tcp-rst`"  ]; then
+        if [ "$(echo ${result} | grep tcp-rst)"  ]; then
                  echo "ERROR: $domain is broken and send's TCP RST for $http://$uri"
         fi
 
